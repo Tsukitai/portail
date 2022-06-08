d57fc92ffb1151ee37c7a6dbb47441b7acca377f
Tsukitai <103410207+Tsukitai@users.noreply.github.com>
2022-06-08 09:17:44 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,6 @@
 input.onButtonPressed(Button.A, function () {
-    pins.digitalWritePin(DigitalPin.P14, 1)
+	
 })
 input.onButtonPressed(Button.B, function () {
-    pins.digitalWritePin(DigitalPin.P14, 0)
-})
-basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P0) == 0) {
-        pins.digitalWritePin(DigitalPin.P13, 0)
-    } else {
-        pins.digitalWritePin(DigitalPin.P13, 1)
-    }
+	
 })
