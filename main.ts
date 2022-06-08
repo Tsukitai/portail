a3e50e454be2133d7ffea60bbeb3986fa2ba3f3c
Tsukitai <103410207+Tsukitai@users.noreply.github.com>
2022-06-08 09:25:53 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 input.onButtonPressed(Button.A, function () {
     servos.P2.setAngle(20)
+    pins.digitalWritePin(DigitalPin.P14, 1)
 })
 input.onButtonPressed(Button.B, function () {
     servos.P2.setAngle(130)
+    pins.digitalWritePin(DigitalPin.P14, 0)
 })
